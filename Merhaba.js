1ac71420495e5699a595ee08edc0b52ad85181ce
albarto2021 <81423967+albarto2021@users.noreply.github.com>
2021-06-12 23:48:39 +0300

    Day1

---
+++ Merhaba.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Merhaba() {
+ return (
+  <View>
+   <Text style={stiller.selam}>Merhaba</Text>
+  </View>
+ )
+}
+
+const stiller = StyleSheet.create({
+ selam: {
+  color: "purple",
+  fontSize: 30,
+ }
+})
+
